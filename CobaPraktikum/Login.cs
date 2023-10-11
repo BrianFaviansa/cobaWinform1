ac7361726ea0936ae42c32612023ba2b33991ede
Brian Faviansa <114897062+BrianFaviansa@users.noreply.github.com>
2023-10-11 18:44:47 +0700

    Add files via upload

---
+++ CobaPraktikum/Login.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CobaPraktikum
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "admin" && textBox2.Text == "admin") 
+            {
+                new Form1().Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Inputan salah");
+            }
+        }
+    }
+}
